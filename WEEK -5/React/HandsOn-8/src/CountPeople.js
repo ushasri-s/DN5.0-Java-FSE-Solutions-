7ff58e6ffc171bb4a9a75d04b3f08f03b9dc563e
ushasri-s <ushasrisakinala2006@gmail.com>
2026-07-23 23:03:57 +0530

    Week 5  React HandsOn 8 - React State Counte

---
+++ WEEK -5/React/HandsOn-8/src/CountPeople.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class CountPeople extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            entrycount: 0,
+            exitcount: 0
+        };
+    }
+
+    updateEntry = () => {
+        this.setState((prevState) => ({
+            entrycount: prevState.entrycount + 1
+        }));
+    };
+
+    updateExit = () => {
+        this.setState((prevState) => ({
+            exitcount: prevState.exitcount + 1
+        }));
+    };
+
+    render() {
+        return (
+            <div style={{ textAlign: "center", marginTop: "50px" }}>
+
+                <button onClick={this.updateEntry}>
+                    Login
+                </button>
+
+                <span style={{ margin: "20px" }}>
+                    {this.state.entrycount} People Entered!!
+                </span>
+
+                <br /><br />
+
+                <button onClick={this.updateExit}>
+                    Exit
+                </button>
+
+                <span style={{ margin: "20px" }}>
+                    {this.state.exitcount} People Left!!
+                </span>
+
+            </div>
+        );
+    }
+}
+
+export default CountPeople;
