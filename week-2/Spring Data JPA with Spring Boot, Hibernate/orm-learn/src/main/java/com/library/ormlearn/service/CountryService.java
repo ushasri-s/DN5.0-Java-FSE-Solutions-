c06a1d706f81da9abaa3a11acc4ac1f4eaa2e89f
ushasri-s <ushasrisakinala2006@gmail.com>
2026-07-24 18:42:36 +0530

    exercise 1

---
+++ week-2/Spring Data JPA with Spring Boot, Hibernate/orm-learn/src/main/java/com/library/ormlearn/service/CountryService.java
@@ -0,0 +1,23 @@
+package com.library.ormlearn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.ormlearn.model.Country;
+import com.library.ormlearn.repository.CountryRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
