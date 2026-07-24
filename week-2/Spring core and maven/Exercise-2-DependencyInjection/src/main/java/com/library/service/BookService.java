f4fdcebe45fb611d1abe1c40768c5dfcc550d3cc
ushasri-s <ushasrisakinala2006@gmail.com>
2026-07-24 16:59:26 +0530

    Exercise 2 Dependency Injection

---
+++ week-2/Spring core and maven/Exercise-2-DependencyInjection/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void showService() {
+        System.out.println("Book Service is working.");
+        bookRepository.display();
+    }
+}
