b7f19d7684a65a5a935f87ba7022d90cda9d7027
ushasri-s <ushasrisakinala2006@gmail.com>
2026-07-24 11:42:18 +0530

    Week 6 React HandsOn-12 -- ticketbookingapp

---
+++ Week -6/React-HandsOn-12/src/GuestGreeting.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function GuestGreeting() {
+    return (
+        <div>
+            <h1>Please sign up.</h1>
+
+            <h3>Flight Details</h3>
+
+            <ul>
+                <li>Flight: AI202</li>
+                <li>From: Hyderabad</li>
+                <li>To: Delhi</li>
+                <li>Time: 10:00 AM</li>
+            </ul>
+        </div>
+    );
+}
+
+export default GuestGreeting;
---
+++ Week -6/React-HandsOn-12/src/UserGreeting.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function UserGreeting() {
+    return (
+        <div>
+            <h1>Welcome back</h1>
+
+            <h3>Flight Booking</h3>
+
+            <p>You can now book your tickets.</p>
+        </div>
+    );
+}
+
+export default UserGreeting;
